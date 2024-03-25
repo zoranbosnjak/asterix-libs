155d1ed41c6ed8c762b34817b864e416763caec8
Zoran Bošnjak <zoran.bosnjak@sloveniacontrol.si>
2024-03-25 10:18:22 +0100

    python, set mypy_path, required for tests

--- libs/python/tests/test_asterix.py
+++ libs/python/tests/test_asterix.py
@@ -14,8 +14,8 @@ TODO:
 '''
 
 def test_raw() -> None:
-    I = Cat.uap.record.spec('010')
     '''
+    I = Cat.uap.record.spec('010')
     i0 = I(0)
     assert i0.to_uinteger() == 0
     assert str(i0.unparse_bits()) == '00000000'
