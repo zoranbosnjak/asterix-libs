f147ad2c4d1fb532dc81629c484668023199e5a4
Zoran Bošnjak <zoran.bosnjak@via.si>
2025-09-29 17:13:58 +0200

    unconditionally regenerate files from upstream

--- update-from-upstream.sh
+++ update-from-upstream.sh
@@ -3,8 +3,8 @@
 # exit when any command fails
 set -e
 
-changes1=$(git status --short nix/aspecs.json)
-if [ -n "$changes1" ]; then
+changes=$(git status --short nix/aspecs.json)
+if [ -n "$changes" ]; then
     echo "Error: local changes in nix/aspecs.json"
     exit 1
 fi
@@ -12,13 +12,10 @@ fi
 # update upstream reference
 nix-shell -p nix-prefetch-scripts --run "nix-prefetch-git https://github.com/zoranbosnjak/asterix-specs.git > nix/aspecs.json"
 
-changes2=$(git status --short nix/aspecs.json)
-if [ -n "$changes2" ]; then
-    # run all regenerate scripts
-    for i in $(find libs/ -type f | grep regenerate.sh); do
-        cd $(dirname $i)
-        ./regenerate.sh
-        cd -
-    done;
-fi
+# run all regenerate scripts
+for i in $(find libs/ -type f | grep regenerate.sh); do
+    cd $(dirname $i)
+    ./regenerate.sh
+    cd -
+done;
 
